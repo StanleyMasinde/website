18cebf1a9a0b885d7cf686b0460f1ddccaa163c4
= <stanleyloren@gmail.com>
2019-12-04 20:23:04 +0300

    icons, theme scss

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navigation></navigation>
     <router-view />
+    <mainfooter></mainfooter>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss"></style>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import navigation from "./components/Nav";
+import mainfooter from "./components/Footer";
+export default {
+  components: {
+    mainfooter,
+    navigation
   }
-}
-</style>
+};
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <!-- Footer -->
+    <footer class="footer text-center">
+      <div class="container">
+        <div class="row">
+          <!-- Footer Location -->
+          <div class="col-lg-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Location</h4>
+            <p class="lead mb-0">
+              Nairobi, Kenya
+            </p>
+          </div>
+
+          <!-- Footer Social Icons -->
+          <div class="col-lg-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Around the Web</h4>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <font-awesome-icon :icon="['fab', 'facebook-f']" />
+            </a>
+            <a
+              title="@StanleyMasinde_"
+              class="btn btn-outline-light btn-social mx-1"
+              href="#"
+            >
+              <font-awesome-icon :icon="['fab', 'twitter']" />
+            </a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+            </a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <font-awesome-icon :icon="['fab', 'github']" />
+            </a>
+          </div>
+
+          <!-- Footer About Text -->
+          <div class="col-lg-4">
+            <h4 class="text-uppercase mb-4">About Stanley Masinde</h4>
+            <p class="lead mb-0">
+              I am a kenya based fullstack developer. See my work.
+              <a target="_blank" href="http://github.com/stannlee">Github</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Copyright Section -->
+    <section class="copyright py-4 text-center text-white">
+      <div class="container">
+        <small
+          >Copyright &copy; Stanley Masinde
+          {{ new Date().getFullYear() }}</small
+        >
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
