b867ae01809637b70903886ef9d538b519f700c7
= <stanleyloren@gmail.com>
2019-12-04 15:33:40 +0300

    innitial commit

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+        <h1>Hi there visitor 😁?</h1>
+        <p>Check back soon to know more about me 🤗. See you around</p>
+
+        <div class="card">
+            <div class="card-body">
+                test card
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
