21c2be6c261da8e28d562968c0b86728a15949fa
Stanley Masinde <stanleyloren@gmail.com>
2020-01-11 20:09:30 +0300

    register service worker, link manifest

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 
+// Regsiter sw
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register("sw.js")
+    .then()
+    .catch();
+}
+
 Vue.config.productionTip = false;
 
 new Vue({
